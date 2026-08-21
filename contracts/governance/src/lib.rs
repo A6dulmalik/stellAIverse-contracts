6762d37e0b1221ec488d05465ac453d003f34465
akargi <aahmadkargi@gmail.com>
2026-08-21 15:42:06 +0100

    feat(governance): update module errors

--- contracts/governance/src/lib.rs
+++ contracts/governance/src/lib.rs
@@ -1,5 +1,4 @@
 #![no_std]
-extern crate alloc;
 
 pub mod contract;
 pub mod errors;
@@ -13,7 +12,3 @@ pub use contract::GovernanceContract;
 pub use errors::GovernanceError;
 pub use types::*;
 pub use utils::*;
-
-// Re-export the contract types for external use
-#[allow(unused_imports)]
-use soroban_sdk::contract;
